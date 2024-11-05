7c268965cf0040899c88716497e351a52f10686d
Deamon23 <s.d2003@yandex.ru>
2024-11-05 17:38:38 +0700

    fix

--- script.js
+++ script.js
@@ -5,40 +5,43 @@ function appendToDisplay(value) {
 
     const lastChar = expression[expression.length - 1];
     if ((lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') && (value === '+' || value === '-' || value === '*' || value === '/')) {
-        return;
+        return; 
     }
 
     if (expression.length > 0 && (value === '+' || value === '-' || value === '*' || value === '/')) {
         expression += ' '; 
     }
+
+    expression += value; 
+
+    updateDisplay();
     
-    expression += value;
-
     if (expression.length > 1 && !isNaN(lastChar) && (value === '+' || value === '-' || value === '*' || value === '/')) {
-        expression += ' '; 
+        expression += ' ';
     }
-
-    updateDisplay(); 
 }
 
 function clearDisplay() {
-    expression = ''; 
-    display.innerHTML = ''; 
+    expression = '';
+    display.innerHTML = '';
 }
 
 function removeLast() {
-    expression = expression.slice(0, -1); 
-    updateDisplay(); 
+    expression = expression.slice(0, -1);
+    while (expression.endsWith(' ')) {
+        expression = expression.slice(0, -1); 
+    }
+    updateDisplay();
 }
 
 function calculateResult() {
     try {
-        let result = eval(expression.replace(/\s+/g, ''));
+        let result = eval(expression.replace(/\s+/g, '')); 
         result = Math.round(result * 100) / 100; 
-        display.innerHTML = `<span style="color: black;">${result}</span>`;
+        display.innerHTML = `<span style="color: black;">${result}</span>`; 
         expression = result.toString(); 
     } catch (error) {
-        expression = '';
+        expression = ''; 
     }
 }
 
@@ -46,5 +49,5 @@ function updateDisplay() {
     const parts = expression.split(' ').map((part, index) => {
         return `<span style="color: ${index === expression.split(' ').length - 1 ? 'black' : 'gray'};">${part}</span>`;
     });
-    display.innerHTML = parts.join(' ');
+    display.innerHTML = parts.join(' '); 
 }
